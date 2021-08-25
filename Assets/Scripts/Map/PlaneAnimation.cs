475b68371630cf40096cf2acb2a1b9af29f67b41
cccrizzz <ccccrizzzz@gmail.com>
2021-08-25 18:09:47 +0800

    Stylized scene

---
+++ Assets/Scripts/Map/PlaneAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneAnimation : MonoBehaviour
+{
+
+
+    public float speed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.RotateAround(transform.position, new Vector3(0,1,0), 0.1f * speed);    
+    }
+}
--- Assets/Scripts/Map/ScrollingTexture.cs
+++ Assets/Scripts/Map/ScrollingTexture.cs
@@ -7,22 +7,48 @@ public class ScrollingTexture : MonoBehaviour
 
 
     public bool on;
+
+    public bool Y;
+
+
+
     float xOffset;
     float animSpeed;
+
+
 
     Renderer rendererRef;
 
     void Start()
     {
+
+        
         rendererRef = GetComponent<Renderer>();
-        animSpeed = 0.5f;
+        animSpeed = -0.5f;
     }
 
     void Update()
     {
+        
+
+
+
         xOffset = Time.time * animSpeed;
-        rendererRef.material.mainTextureOffset = new Vector2(xOffset, xOffset);
+
+        if (Y)
+        {
+            rendererRef.material.mainTextureOffset = new Vector2(0, -xOffset);    
+        }
+        else
+        {
+            rendererRef.material.mainTextureOffset = new Vector2(xOffset, 0);
+        }
+
         if (xOffset > 1) xOffset -= 1; // This prevents inaccuracies when you go over 1.0
-            
+
+
+
     }
+
+
 }
